113fc174178640a9d418cc0c416a2ac8a0c9a484
Andrew Berls <andrew.berls@gmail.com>
2012-10-23 20:45:07 -0700

    Initial Rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swapmeet::Application.config.secret_token = '59ebb41403fbda89db59182267379999cf79fc040f6077072b2c3615b67e39209a547fce65cdc90090ba61f176bdf8228888a366b107fb9398eb4e8461d8bb58'
