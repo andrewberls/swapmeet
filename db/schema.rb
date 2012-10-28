2d0db7495887b661843c19d40c887a35804f8fde
Jacopo Corbetta <jacopo@cs.ucsb.edu>
2012-10-28 14:55:44 -0700

    The new db schema with the password digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121028015226) do
+ActiveRecord::Schema.define(:version => 20121028214229) do
 
   create_table "offers", :force => true do |t|
     t.string   "title"
@@ -32,8 +32,9 @@ ActiveRecord::Schema.define(:version => 20121028015226) do
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
