59250c7648dadaaea5532328b6c87735e79574d6
Jacopo Corbetta <jacopo@cs.ucsb.edu>
2012-10-27 19:32:52 -0700

    Temporary scaffolding for offers
    Just so I can have something to base my work on.

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,4 +4,12 @@ class Offer < ActiveRecord::Base
   belongs_to :user
   has_many :responses
   has_many :bids, through: :responses
+
+  after_initialize :init
+  def init
+    # Allows Offer objects to be constructed with just the data
+    # For now, all offers belong to the first user in the DB 
+    # FIXME: properly associate the Offer with the user logged in
+    self.user = User.first!
+  end
 end
