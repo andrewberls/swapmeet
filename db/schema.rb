0eae8a81b0de669f382ae1432aadd77efcc44731
Jacopo Corbetta <jacopo@cs.ucsb.edu>
2012-10-27 18:54:26 -0700

    Unique username and emails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121025184132) do
+ActiveRecord::Schema.define(:version => 20121028015226) do
 
   create_table "offers", :force => true do |t|
     t.string   "title"
@@ -35,5 +35,8 @@ ActiveRecord::Schema.define(:version => 20121025184132) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
