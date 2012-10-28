74e1c6e54df61a508361fa3f444e25d4921ff2c6
Jacopo Corbetta <jacopo@cs.ucsb.edu>
2012-10-28 14:53:09 -0700

    Don's user changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :username, :email
+  attr_accessible :username, :email, :password, :password_confirmation
   has_many :offers
+  has_secure_password
 
   validates :username, :uniqueness => true
   validates :email, :uniqueness => true
