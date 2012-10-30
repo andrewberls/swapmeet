43824b6d3349c4169b54a79e8991bbe16b733280
Andrew Berls <andrew.berls@gmail.com>
2012-10-29 21:03:38 -0700

    Getting error messages to work properly for signup

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,7 +1,7 @@
 module DeviseHelper
   def devise_error_messages!
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    header   = content_tag :p, "Something went wrong - please check your fields and try again."
-    content_tag :div, header + raw(messages), class: 'alert alert-error'
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }
+    header   = content_tag :p, "#{pluralize(messages.count, 'error')} prevented the form from being saved:"
+    content_tag :div, header + raw(messages.join), class: 'alert alert-error'
   end
 end
