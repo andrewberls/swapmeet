5c9fbb5e7921473f046eb3d2606d1e5e1109be0e
Andrew Berls <andrew.berls@gmail.com>
2012-11-05 18:06:03 -0800

    More managed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,29 @@ end
 mark = " -> "
 
 
-puts "\nStarting seed generation. Configuration: "
+puts "\nCreating managed seeds.. "
+
+admin_user = User.new(username: "admin", email: "admin@admin.com", password: "password", password_confirmation: "password")
+admin_user.save!
+good_user  = User.new(username: "gooduser", email: "gooduser@email.com", password: "password", password_confirmation: "password")
+good_user.save!
+
+awesome_offer = Offer.new(title: "Awesome offer", description: "I need to get rid of my really awesome stuff.") do |offer|
+  offer.user = random_user
+end
+
+cool_bid = Offer.new(title: "Cool bid", description: "I will give you really cool stuff for your awesome stuff") do |offer|
+  offer.user = good_user
+end
+
+awesome_offer.save!
+awesome_offer.responses.new(bid: cool_bid) { |resp| resp.status = 'open' }.save!
+
+
+
+
+puts "Done."
+puts "Starting random seed generation. Configuration: "
 puts "#{mark}user count:  #{USER_COUNT}"
 puts "#{mark}offer count: #{OFFER_COUNT}"
 puts "#{mark}bid count:   #{BID_COUNT}"
@@ -26,8 +48,6 @@ beginning_time = Time.now
 #--------------------
 # Users
 #--------------------
-User.create(username: "admin", email: "admin@admin.com", password: "password", password_confirmation: "password")
-
 USER_COUNT.times do |i|
   User.create!(username: "user#{i}", email: "user#{i}@fake.com", password: "password", password_confirmation: "password")
 end
@@ -63,7 +83,8 @@ BID_COUNT.times do |i|
 
   bid = Offer.new(title: "test-bid#{i}", description: LiterateRandomizer.sentence) do |offer|
     offer.user = bid_user
-  end.save!
+  end
+  bid.save!
 
   response = parent_offer.responses.create(bid: bid)
 end
