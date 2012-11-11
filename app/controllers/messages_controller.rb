29980c6236455ec59f0d1dd0aa5dc1142b2ebc07
Andrew Berls <andrew.berls@gmail.com>
2012-11-11 14:55:33 -0800

    Fixing action-btns

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController
   before_filter :authenticate_user!
 
-  
+
   def create
-    @message = Message.new(recipient_id: params[:message][:recipient_id], sender_id: current_user.id, content: params[:message][:content] )
-    
+    msg = params[:message]
+    @message = Message.new(recipient_id: msg[:recipient_id], sender_id: current_user.id, content: msg[:content] )
+
     respond_to do |format|
       if @message.save
         format.html { redirect_to messages_inbox_path, notice: 'Message sent.' }
@@ -15,31 +16,31 @@ class MessagesController < ApplicationController
       end
     end
   end
-  
+
   def show
     #raise "params = #{params.inspect}"
     user = User.find(params[:id])
     @messages = Message.all_between(current_user, user)
     @messages.each { |m| m.mark_as_read! }
-    
+
     #new message at bottom
     @message = Message.new(:recipient_id => params[:id])
     if @message.recipient.nil?
       flash[:error] = 'Invalid recipient.'
       redirect_to messages_path
-    else 
+    else
       respond_to do |format|
         format.html # show.html.erb
         format.json { render json: @message }
       end
     end
-    
+
 
   end
-  
+
   def inbox
     @messages = Message.unread_message_summary_for_user(current_user)
-    
+
     respond_to do |format|
       format.html # inbox.html.erb
       format.json { render json: @messages }
@@ -53,5 +54,5 @@ class MessagesController < ApplicationController
       format.json { render json: @messages }
     end
   end
-  
+
 end
