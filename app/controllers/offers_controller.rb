1804db868a4377ac674e16d663b01c13739cfebf
Don Voita <dvoita@gmail.com>
2012-11-11 23:22:47 -0800

    Added reusable items

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -15,6 +15,7 @@ class OffersController < ApplicationController
       return redirect_to @parent_offer if current_user == @parent_offer.user
       @offer = Offer.new
       
+      # Allow user to choose any of their items not already bid for this trade.
       bid_ids = Response.where(:offer_id => params[:id]).pluck("bid_id")
       @reusable_offers = Offer.where(['id not in (?)', bid_ids]).where(:user_id => current_user.id)
     else
