862397c76de5c30a6f3d45fe7d08d2a3234f0ed8
Andrew Berls <andrew.berls@gmail.com>
2012-11-13 15:14:13 -0800

    Cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,14 +25,6 @@ def random_offer_id
   rand(1..Offer.count)
 end
 
-def random_user
-  User.find(random_user_id)
-end
-
-def random_offer
-  Offer.find(random_offer_id)
-end
-
 mark = " -> "
 
 
@@ -79,7 +71,8 @@ beginning_time = Time.now
 print "Creating users..."
 user_start = Time.now
 USER_COUNT.times do |i|
-  query = %Q{INSERT INTO `users` (`created_at`, `current_sign_in_at`, `current_sign_in_ip`, `down_ratings`, `email`, `encrypted_password`, `last_sign_in_at`, `last_sign_in_ip`, `remember_created_at`, `reset_password_sent_at`, `reset_password_token`, `sign_in_count`, `up_ratings`, `updated_at`, `username`) VALUES ('#{Time.now.to_s(:db)}', NULL, NULL, 0, 'user#{i}@fake.com', '$2a$10$pvCaaiZW5O43M/r7xCeKkOJ3Vlgv8pUgvl77C6SFrot4Ml1eBeN1a', NULL, NULL, NULL, NULL, NULL, 0, 0, '#{Time.now.to_s(:db)}', 'user#{i}')}
+  time  = Time.now.to_s(:db)
+  query = %Q{INSERT INTO `users` (`created_at`, `current_sign_in_at`, `current_sign_in_ip`, `down_ratings`, `email`, `encrypted_password`, `last_sign_in_at`, `last_sign_in_ip`, `remember_created_at`, `reset_password_sent_at`, `reset_password_token`, `sign_in_count`, `up_ratings`, `updated_at`, `username`) VALUES ('#{time}', NULL, NULL, 0, 'user#{i}@fake.com', '$2a$10$pvCaaiZW5O43M/r7xCeKkOJ3Vlgv8pUgvl77C6SFrot4Ml1eBeN1a', NULL, NULL, NULL, NULL, NULL, 0, 0, '#{time}', 'user#{i}')}
 
   execute(query)
 end
@@ -93,9 +86,9 @@ print "Done. (#{Time.now - user_start}s)\n"
 offer_start = Time.now
 print "Creating offers..."
 OFFER_COUNT.times do |i|
-  desc    = LiterateRandomizer.sentence
-  user_id = random_user_id
-  query = %Q{INSERT INTO `offers` (`created_at`, `description`, `image_content_type`, `image_file_name`, `image_file_size`, `image_updated_at`, `title`, `updated_at`, `user_id`) VALUES ('#{Time.now.to_s(:db)}', "#{desc}", NULL, NULL, NULL, NULL, 'test-offer#{i}', '#{Time.now.to_s(:db)}', #{user_id})}
+  desc  = LiterateRandomizer.sentence
+  time  = Time.now.to_s(:db)
+  query = %Q{INSERT INTO `offers` (`created_at`, `description`, `image_content_type`, `image_file_name`, `image_file_size`, `image_updated_at`, `title`, `updated_at`, `user_id`) VALUES ('#{time}', "#{desc}", NULL, NULL, NULL, NULL, 'test-offer#{i}', '#{time}', #{random_user_id})}
 
   execute(query)
 end
@@ -108,6 +101,7 @@ print "Done. (#{Time.now - offer_start}s)\n"
 #--------------------
 # Extra bit of shuffling to ensure only parent offers receive bids,
 # and users dont bid on their own offers
+
 print "Creating bids..."
 
 bid_start = Time.now
@@ -124,11 +118,13 @@ BID_COUNT.times do |i|
   end
 
   desc      = LiterateRandomizer.sentence
-  bid_query = %Q{INSERT INTO `offers` (`created_at`, `description`, `image_content_type`, `image_file_name`, `image_file_size`, `image_updated_at`, `title`, `updated_at`, `user_id`) VALUES ('#{Time.now.to_s(:db)}', "#{desc}", NULL, NULL, NULL, NULL, 'test-bid#{i}', '#{Time.now.to_s(:db)}', #{bid_user_id})}
+  time      = Time.now.to_s(:db)
+  bid_query = %Q{INSERT INTO `offers` (`created_at`, `description`, `image_content_type`, `image_file_name`, `image_file_size`, `image_updated_at`, `title`, `updated_at`, `user_id`) VALUES ('#{time}', "#{desc}", NULL, NULL, NULL, NULL, 'test-bid#{i}', '#{time}', #{bid_user_id})}
   execute(bid_query)
 
   bid_id = Offer.last.id # TODO
-  resp_query = %Q{INSERT INTO `responses` (`bid_id`, `bidder_rated`, `created_at`, `offer_id`, `offerer_rated`, `status`, `updated_at`) VALUES (#{bid_id}, 0, '#{Time.now.to_s(:db)}', #{parent_offer_id}, 0, 'open', '#{Time.now.to_s(:db)}')}
+  time   = Time.now.to_s(:db)
+  resp_query = %Q{INSERT INTO `responses` (`bid_id`, `bidder_rated`, `created_at`, `offer_id`, `offerer_rated`, `status`, `updated_at`) VALUES (#{bid_id}, 0, '#{time}', #{parent_offer_id}, 0, 'open', '#{time}')}
   execute(resp_query)
 
 end
