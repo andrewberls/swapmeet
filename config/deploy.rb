90619859a81da2537088d5c16ecba555f9e66853
Sarah Jones <sarah.e.p.jones@gmail.com>
2012-11-15 23:42:43 -0800

    5.Capistrano Configure aws credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,12 @@ require "bundler/capistrano"
 
 set :application, "Swapmeet"
 set :repository,  "git@github.com:andrewberls/swapmeet.git"
-
+set :scm, :git 
 set :branch, "master"
+
 set :deploy_to, "/home/ubuntu"
 set :user, "ubuntu"
 
-set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, "swapmeet.dnsdynamic.net"                          # Your HTTP server, Apache/etc
 role :app, "swapmeet.dnsdynamic.net"                          # This may be the same as your `Web` server
@@ -18,17 +17,23 @@ role :db,  "swapmeet.dnsdynamic.net", :primary => true # This is where Rails mig
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
+#Move the aws credentials from where RightScript put them to where it should be
+after "deploy:update_code", "deploy:symlink_aws.yml"
+
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
- namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
- end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  task :symlink_aws do
+    run "ln -nfs #{shared_path}/config/aws.yml #{release_path}/config/aws.yml"
+  end
+end
 
 
 ssh_options[:keys] = ["~/.ssh/our-key-from-rightscale"]
