d636d4e5e0ebaedb9d2124da2c784cb154c4688e
Jacopo Corbetta <jacopo@cs.ucsb.edu>
2012-12-01 13:11:50 -0800

    Untested funkload test

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -264,29 +264,26 @@ class OffersController < ApplicationController
       end
   end
 
-  def get_rand_offer
-    current_user.offers.sample
-  end
-
   def fix_666_params
     rand_offer = nil
     if params.has_key?(:id)
-      if params[:id] == 666
-        rand_offer = get_rand_offer()
-        params[:id] = rand_offer.id
+      if params[:id] == '666'
+        rand_offer = current_user.offers.sample
+        params[:id] = rand_offer.id.to_s()
       end
     end
     if params.has_key?(:offer_id)
-      if params[:offer_id] == 666
-        if rand_offer
-          rand_offer = get_rand_offer()
+      if params[:offer_id] == '666'
+        if rand_offer.nil?
+          rand_offer = current_user.offers_with_bids.sample
         end
-        params[:offer_id] = rand_offer.id
+        params[:offer_id] = rand_offer.id.to_s()
       end
     end
     if params.has_key?(:bid_id)
-      if params[:bid_id] == 666
-        params[:bid_id] = rand_offer.bids.sample.id
+      if params[:bid_id] == '666'
+        debugger
+        params[:bid_id] = rand_offer.bids.sample.id.to_s()
       end
     end
   end
---
+++ funkload/test_CriticalPath.py
@@ -0,0 +1,95 @@
+# -*- coding: iso-8859-15 -*-
+"""critical_path FunkLoad test
+
+$Id: $
+"""
+import unittest
+from funkload.FunkLoadTestCase import FunkLoadTestCase
+from webunit.utility import Upload
+from funkload.utils import Data
+#from funkload.utils import xmlrpc_get_credential
+
+import random
+
+class CriticalPath(FunkLoadTestCase):
+    """Test Swapmeet's critical path
+
+    This test use a configuration file CriticalPath.conf.
+    """
+
+    def setUp(self):
+        """Setting up test."""
+        self.logd("setUp")
+        self.server_url = self.conf_get('main', 'url')
+        # XXX here you can setup the credential access like this
+        # credential_host = self.conf_get('credential', 'host')
+        # credential_port = self.conf_getInt('credential', 'port')
+        # self.login, self.password = xmlrpc_get_credential(credential_host,
+        #                                                   credential_port,
+        # XXX replace with a valid group
+        #                                                   'members')
+
+    def test_critical_path(self):
+        # The description should be set in the configuration file
+        server_url = self.server_url
+        # begin of test ---------------------------------------------
+
+        # /tmp/tmpxcCX8v_funkload/watch0005.request
+        self.post(server_url+"/users/sign_in", params=[
+            ['utf8', '\xe2\x9c\x93'],
+            ['user[login]', 'user'+str(random.randint(20,1000))],
+            ['user[password]', 'password'],
+            ['user[remember_me]', '0'],
+            ['commit', 'Log in']],
+            description="Post /users/sign_in")
+        # /tmp/tmpxcCX8v_funkload/watch0007.request
+        self.get(server_url+"/offers/new",
+            description="Get /offers/new")
+        # /tmp/tmpxcCX8v_funkload/watch0008.request
+        self.post(server_url+"/offers", params=[
+            ['utf8', '\xe2\x9c\x93'],
+            ['offer[title]', 'TestingOffer'],
+            ['offer[description]', 'Just for testing'],
+            ['offer[image]', Upload("")],
+            ['offer[tag_list]', 'test'],
+            ['commit', 'Create Offer']],
+            description="Post /offers")
+        # /tmp/tmpxcCX8v_funkload/watch0007.request
+        self.get(server_url+"/offers/666/bid",
+            description="Load the new bid page")
+        # /tmp/tmpxcCX8v_funkload/watch0008.request
+        self.post(server_url+"/offers/666/bid", params=[
+            ['utf8', '\xe2\x9c\x93'],
+            ['offer[title]', 'TestingBid'],
+            ['offer[description]', 'Just for testing'],
+            ['offer[image]', Upload("")],
+            ['offer[tag_list]', 'test'],
+            ['commit', 'Bid on something']],
+            description="Post /offers")
+        # /tmp/tmpxcCX8v_funkload/watch0034.request
+        self.get(server_url+"/dashboard",
+            description="Get /dashboard")
+        # /tmp/tmpxcCX8v_funkload/watch0035.request
+        self.get(server_url+"/offers/666",
+            description="Get random offer")
+        # /tmp/tmpxcCX8v_funkload/watch0036.request
+        self.post(server_url+"/offers/666/accept/666", params=[
+            ['utf8', '\xe2\x9c\x93'],
+            ],
+            description="Accept a bid")
+        # /tmp/tmpxcCX8v_funkload/watch0036.request
+        self.post(server_url+"/offers/666/complete/666", params=[
+            ['utf8', '\xe2\x9c\x93'],
+            ],
+            description="Complete a bid")
+
+        # end of test -----------------------------------------------
+
+    def tearDown(self):
+        """Setting up test."""
+        self.logd("tearDown.\n")
+
+
+
+if __name__ in ('main', '__main__'):
+    unittest.main()
