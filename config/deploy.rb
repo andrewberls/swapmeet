6153a91b8d5a37b5cecd95e8e83f088810203886
Andrew Berls <andrew.berls@gmail.com>
2012-12-05 20:12:47 -0800

    Clean up cap deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,27 @@
 require "bundler/capistrano"
 
-#use -s branch=BRANCHNAME to change branches
+# Use -s branch=BRANCHNAME to change branches
 
-set :application, "Swapmeet"
-#set :repository,  "git@github.com:andrewberls/swapmeet.git"
-#use read only to avoid SSH key issues
+# Use git read-only to avoid SSH key issues
 set :repository, "git://github.com/andrewberls/swapmeet.git"
-set :scm, :git 
-
+set :scm, :git
 set :branch, "master"
 
 set :deploy_to, "/home/ubuntu"
 set :user, "ubuntu"
 set :use_sudo, false
 
+role :web, "swapmeetapp1.dnsdynamic.net", "swapmeetapp2.dnsdynamic.net"    # Your HTTP server, Apache/etc
+role :app, "swapmeetapp1.dnsdynamic.net", "swapmeetapp2.dnsdynamic.net"    # This may be the same as your `Web` server
+role :db,  "swapmeetapp2.dnsdynamic.net", :primary => true                 # This is where Rails migrations will run
+#role :db,  "swapmeetdb.dnsdynamic.net", :no_release => true #actual db server
 
-  role :web, "swapmeetapp1.dnsdynamic.net", "swapmeetapp2.dnsdynamic.net"                          # Your HTTP server, Apache/etc
-  role :app, "swapmeetapp1.dnsdynamic.net", "swapmeetapp2.dnsdynamic.net"                         # This may be the same as your `Web` server
-  role :db,  "swapmeetapp2.dnsdynamic.net", :primary => true # This is where Rails migrations will run
-  #role :db,  "swapmeetdb.dnsdynamic.net", :no_release => true #actual db server
-  #role :db,  "your slave db-server here"
-
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-#Move the aws credentials from where RightScript put them to where it should be
+# Move the AWS credentials from where RightScript put them to where it should be
 after "deploy:update_code", "deploy:symlink_aws"
 
-#migrate
+# Migrate
 after "deploy:update_code", "deploy:migrate"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
