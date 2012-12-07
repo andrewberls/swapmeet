588ad900ca90ee801a5fb21bd538021eb1566c10
Andrew Berls <andrew.berls@gmail.com>
2012-12-06 18:25:37 -0800

    Set db server to app1

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,8 +13,7 @@ set :use_sudo, false
 
 role :web, "swapmeetapp1.dnsdynamic.net", "swapmeetapp2.dnsdynamic.net"    # Your HTTP server, Apache/etc
 role :app, "swapmeetapp1.dnsdynamic.net", "swapmeetapp2.dnsdynamic.net"    # This may be the same as your `Web` server
-role :db,  "swapmeetapp2.dnsdynamic.net", :primary => true                 # This is where Rails migrations will run
-#role :db,  "swapmeetdb.dnsdynamic.net", :no_release => true #actual db server
+role :db,  "swapmeetapp1.dnsdynamic.net", :primary => true                 # This is where Rails migrations will run
 
 # Move the AWS credentials from where RightScript put them to where it should be
 after "deploy:update_code", "deploy:symlink_aws"
